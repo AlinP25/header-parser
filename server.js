815361bb0a28e0ca78f95326c26b227966cc65b0
Alin Pisica <alinp2508@gmail.com>
2016-07-07 23:20:56 +0300

    Cleaned up the code

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var express = require("express");
 var app = express();
 
 app.get('/', function (req, res) {
-    var lang = req.headers['accept-language'].split(',')[0];
+    /*var lang = req.headers['accept-language'].split(',')[0];
     var firstSplit = req.headers['user-agent'].split(') ')[0]
     var secondSplit = firstSplit.split('(');
     var os = secondSplit[1];
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAdress;
-    console.log(ip);
+    console.log(ip);*/
     res.end(JSON.stringify({
-        'ip': ip,
-        'language': lang,
-        'software': os,
+        'ip': (req.headers['x-forwarded-for'] || req.connection.remoteAdress),
+        'language': req.headers['accept-language'].split(',')[0],
+        'software': req.headers['user-agent'].split(') ')[0].split('(')[1],
     }));
 });
 
